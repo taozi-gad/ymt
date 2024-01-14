73476f4ba70e46f72d5692583db41330401afc85
ymt <941151407@qq.com>
2024-01-14 14:38:19 +0800

    初次提交

---
+++ JAVA_Study/day1/src/com/tju/Literal/LiteralDemo.java
@@ -0,0 +1,24 @@
+package com.tju.Literal;
+
+public class LiteralDemo {
+    public static void main(String[] args) {
+        // 注释：
+        // 单行注释
+        /*
+        多行注释
+         */
+
+
+        // int
+        System.out.println(999);
+
+        // \n
+//        System.out.println('\n');
+        System.out.println('a');
+        System.out.println("中国");
+        System.out.println(true);
+        System.out.println(false);
+
+
+    }
+}
---
+++ JAVA_Study/day1/src/com/tju/variable/Variable.java
@@ -0,0 +1,24 @@
+package com.tju.variable;
+
+/**
+ * @author ymt
+ */
+
+public class Variable {
+    public static void main(String[] args) {
+        // int
+        int age = 23;
+        double score = 33.5;
+        System.out.println(age);
+        System.out.println("----------------------------");
+        System.out.println(score);
+
+        int age2 = age + 1;
+        System.out.println(age2);
+        age2 += 1;
+        System.out.println(age2);
+
+
+    }
+}
+
