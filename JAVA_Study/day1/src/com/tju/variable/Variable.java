7ec117e73b7b1493a6a277b457172e1322b8e906
ymt <941151407@qq.com>
2024-01-14 15:01:25 +0800

    学习强制类型转换

--- JAVA_Study/day1/src/com/tju/variable/Variable.java
+++ JAVA_Study/day1/src/com/tju/variable/Variable.java
@@ -18,6 +18,17 @@ public class Variable {
         age2 += 1;
         System.out.println(age2);
 
+        byte b1 = 10;
+        byte b2 = 20;
+        int b3 = b1 + b2;
+        System.out.println(b3);
+
+        // 强制类型转换
+        int a = 15000;
+        //出现报错后可以使用alt + enter
+        byte b = (byte) a;
+        System.out.println(a);
+        System.out.println(b);
 
     }
 }
